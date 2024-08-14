7c3acfcbbf74c2bbff6d072c194bc6b89eb7b12c
Geethika-Kancharla <geethikak004@gmail.com>
2024-08-14 19:51:35 +0530

    Added Spinners

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import axios from 'axios';
+import { ClipLoader } from 'react-spinners';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart = () => {
     const [chartData, setChartData] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get('http://localhost:8080/countAll')
@@ -30,9 +32,11 @@ const PieChart = () => {
                         },
                     ],
                 });
+                setLoading(false); // Set loading to false when data is fetched
             })
             .catch((error) => {
                 console.error('Error fetching data:', error);
+                setLoading(false); // Set loading to false if there's an error
             });
     }, []);
 
@@ -50,8 +54,14 @@ const PieChart = () => {
 
     return (
         <div className="flex justify-center items-center mt-10 bg-white">
-            <div className="w-full max-w-2xl sm:max-w-3xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl h-80 sm:h-96 md:h-[400px] lg:h-[500px] md:ml-48">
-                {chartData ? <Pie data={chartData} options={options} /> : <p>Loading...</p>}
+            <div className="w-full max-w-2xl sm:max-w-3xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl h-80 sm:h-96 md:h-[400px] lg:h-[500px] md:ml-56">
+                {loading ? (
+                    <div className="flex justify-center items-center h-full">
+                        <ClipLoader color="#ff4500" size={50} /> {/* Replace with the spinner you like */}
+                    </div>
+                ) : (
+                    <Pie data={chartData} options={options} />
+                )}
             </div>
         </div>
     );
